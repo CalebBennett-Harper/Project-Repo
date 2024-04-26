0048960fad996ee5ce8189d8bb4cd944dd13f54c
yukifengg <yuki.2102005@gmail.com>
2024-04-25 21:50:24 -0700

    html css

---
+++ 23f-project-boilerplate-main/flask-app/static/js/index.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code goes here
+    
+    // Example: Change the color of the body background
+    document.body.style.backgroundColor = '#f4f4f4';
+    
+    // Example: Add a click event listener to a button
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
